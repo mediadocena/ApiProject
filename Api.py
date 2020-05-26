f4dc9279d5230a45894deea455e4bf1fc3ad5d95
Alejandro García <45419684+mediadocena@users.noreply.github.com>
2020-05-26 12:43:25 +0200

    Update Api.py

--- Api.py
+++ Api.py
@@ -342,11 +342,11 @@ def GetByCategory(cat):
 #Run app
 @app.after_request
 def after_request(response):
-    response.headers.add('Cache-Control:','no-store')
+    #response.headers.add('Cache-Control:','no-store')
     response.headers.add('Access-Control-Allow-Origin', '*')
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
     return response
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
