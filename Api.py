a0751b77cf5943bc173d12ec4bd35eeb7e05f045
Alejandro García <45419684+mediadocena@users.noreply.github.com>
2020-05-31 15:24:56 +0200

    cors

--- Api.py
+++ Api.py
@@ -364,7 +364,7 @@ def GetByCategory(cat):
 @app.after_request
 def after_request(response):
     #response.headers.add('Cache-Control:','no-store')
-    response.headers.add('Access-Control-Allow-Origin', '*')
+    #response.headers.add('Access-Control-Allow-Origin', '*')
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
     return response
